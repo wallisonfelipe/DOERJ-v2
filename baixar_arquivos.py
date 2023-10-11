6c10a395cfd3588854981e997f7b984e174c4563
bpessanha21 <108680866+bpessanha21@users.noreply.github.com>
2023-10-10 22:42:32 -0300

    Add files via upload

--- baixar_arquivos.py
+++ baixar_arquivos.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from PyPDF2 import PdfMerger
+
 
 def baixar_arquivos(url, endereco):
     #faz requisicao ao servidor
@@ -11,13 +13,16 @@ def baixar_arquivos(url, endereco):
     else:
         resposta.raise_for_status()
 
+
+# Entrar no doerj e clicar na parte branca com botão direito e clica em exibir código fonte / depois pega o link cola aqui na url e tira a pagina
+# pega os arquivo e junta no ilove pdf
 if __name__== "__main__":
-    BASE_URL = 'http://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?k=BEE44B7B-E39DD-4D40-BC29-2067D630F00E{}'
+    BASE_URL = 'http://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?k=606F333F-69DF0-4D68-81C4-928C0E48659E{}'
     OUTPUT_DIR = 'output'
 
 # inclusive range
 start = 1
-stop = 1
+stop = 5
 step = 1
 
 # change stop
@@ -27,5 +32,16 @@ for i in range(start, stop, step):
         nome_arquivo = os.path.join(OUTPUT_DIR, 'doerj_{}.pdf'.format(i))
         baixar_arquivos(BASE_URL.format(i), nome_arquivo)
 
-        #Entrar no doerj e clicar na parte branca com botão direito e clica em exibir código fonte / depois pega o link cola aqui na url e tira a pagina
-        #pega os arquivo e junta no ilove pdf
+merger = PdfMerger()
+
+lista_arquivos = os.listdir("OUTPUT")
+for arquivo in lista_arquivos:
+     if ".pdf" in arquivo:
+          if ".pdf" in arquivo:
+               merger.append(f"OUTPUT/{arquivo}")
+
+merger.write("DOERJ_Final.pdf")
+
+       
+
+
