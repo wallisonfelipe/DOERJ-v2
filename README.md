d72f87ac6d012d4163311f699949bc289095d1d9
wallisonfelipe <wallison_felipe99@hotmail.com>
2023-10-31 08:28:01 -0300

    Subindo script

--- README.md
+++
@@ -1,5 +0,0 @@
-# DOERJ
-
-https://www.youtube.com/watch?v=WzUPnQwSQyk&t=1710s --- BEATIFUL SOUP
-
-https://www.youtube.com/watch?v=8AMNaVt0z_M ---- Como Fazer uma Automação Web Utilizando o Python e o Selenium
---
+++ index.py
@@ -0,0 +1,78 @@
+import requests
+import re
+from urllib.parse import urlparse, parse_qs
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+url_base = "https://www.ioerj.com.br"
+
+
+def get_today_link():
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
+    response = requests.get(url_base + "/portal/modules/conteudoonline/do_ultima_edicao.php", headers=headers)
+    pattern = r"<a\s+href='([^']+)'"
+    sub_link = re.search(pattern, response.text).group(1)
+
+    parsed_url = urlparse(sub_link)
+    query_parameters = parse_qs(parsed_url.query)
+    data_param = query_parameters.get('data', None)
+
+    return "https://www.ioerj.com.br/portal/modules/conteudoonline/do_seleciona_edicao.php?data=" + data_param[0]
+
+def get_file_links(url):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
+    response = requests.get(url, headers=headers)
+    
+    site = BeautifulSoup(response.text, "html.parser")
+    real_links = []
+    links = site.select("#xo-content > font > div > ul > li")
+    for anchor in links:
+        real_links.append("https://www.ioerj.com.br/portal/modules/conteudoonline/" + anchor.select_one("a").get("href"))
+    
+    return real_links
+
+def get_file_from_link(link, name):
+    chromeIntall = ChromeDriverManager().install()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    
+    driver = webdriver.Chrome(service=ChromeService(chromeIntall), options=options)
+    driver.get(link)
+    driver.implicitly_wait(10)
+    
+    driver.execute_script('var hiddenInput = document.createElement("input");hiddenInput.setAttribute("type", "hidden");hiddenInput.setAttribute("value", pd);hiddenInput.setAttribute("id", "pd");document.body.appendChild(hiddenInput);')
+    documentId = driver.find_element("id", "pd").get_attribute("value")
+    driver.close()
+    documentId = documentId[:12] + 'P' + documentId[12:]
+    print(documentId)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
+    response = requests.get("https://www.ioerj.com.br/portal/modules/conteudoonline/mostra_edicao.php?k=" + documentId, headers=headers)
+    with open(name, "wb") as file:
+        file.write(response.content)
+
+today=get_today_link()
+links = get_file_links(today)
+
+mapper = {
+    0: "Poder_Executivo.pdf",
+    1: "Tribunal_de_contas.pdf",
+    2: "Poder_Legislativo.pdf",
+    3: "Municipalidades.pdf",
+    4: "Publicacoes_a_pedido.pdf",
+}
+count = 0
+
+for link in links:
+    get_file_from_link(link, mapper[count])
+    count = count + 1
+
+
+
+
+# #saves the content response in a file
